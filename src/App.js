cb3996b948ed50f9a0389e88bde46420c07498dc
zubair arif <zubarif234@gmail.com>
2022-11-22 23:06:29 +0500

    header and poster comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import Turologo from './assets/turologo.png'
+import humburgur from './assets/humburgur.png'
+import profile from './assets/profile.png'
+import search from './assets/search.png'
+import Container from 'react-bootstrap/Container';
+import { UserOutlined } from '@ant-design/icons';
+import { Input,DatePicker,  Select,  TimePicker  } from 'antd';
 
+
+// import { DatePicker, Space } from 'antd';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='topAdiv'>
+    <a href='#' id='topA'>Turo has launched in New York! Tap to explore cars.</a>
     </div>
   );
 }
+function Head2(){
+  return(
+   
 
-export default App;
+     <Container > 
+
+    <div id='head2'>
+<img src={Turologo} alt='' id='imglogo' />
+<div id='head25'>
+  <button id='head2button'>Become a host </button>
+  <div id='logoheadimgs'>
+    <img src={humburgur} alt=' ' id='logoimgs1'/>
+    <img src={profile} alt=' ' />
+  </div>
+</div>
+    </div>
+    </Container>
+  )
+}
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
+function Poster(){
+  return(
+    <div id='posterdiv'>
+      <div id='inputsdiv'> 
+      <div className='inputparentdiv'>
+
+        <lable className='wherelabel'>Where</lable>
+        {/* <input type='test'/> */}
+        <Input className='whereinputsearch' size="small" placeholder="City, airport, address or hotel"  />
+      </div>
+      <div className='inputparentdiv'>
+        <lable className='wherelabel'>from</lable>
+        <div className='dateTime'>
+
+        <DatePicker className='whereinput' size="small" onChange={onChange} />
+        <TimePicker className='whereinput' size="small" onChange={onChange} />
+        </div>
+        
+      </div>
+      <div className='inputparentdiv'>
+        <lable className='wherelabel'>until</lable>
+        <div className='dateTime'>
+
+        <DatePicker className='whereinput' size="small" onChange={onChange} />
+        <TimePicker className='whereinput' size="small" onChange={onChange} />
+        </div>
+
+      </div>
+      <div id='searchicon' className='inputparentdiv'>
+        <img src={search} alt='' id='searchiconimg'/>
+      </div>
+        <button id='searchbtn'>search for car</button>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export {
+
+  App,
+  Head2,
+  Poster,
+}  
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App,Head2,Poster} from './App';
 import reportWebVitals from './reportWebVitals';
+import Container from 'react-bootstrap/Container';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Container> */}
+
+    <Head2 />
+    <Poster />
+    {/* </Container> */}
   </React.StrictMode>
 );
 
